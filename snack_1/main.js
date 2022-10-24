6aba151cf3ab0ec18cc19e46d40075ec129de863
Francesca-Barcella <pollon.fra@gmail>
2022-10-24 15:07:43 +0200

    snack_1 - creato array e foreach per stam pesi

--- snack_1/main.js
+++ snack_1/main.js
@@ -1,4 +1,4 @@
-alert ('snack_1 funge!')
+//alert ('snack_1 funge!')
 
 /* 
 SSNACK_1
@@ -7,3 +7,74 @@ Crea un array di 10 oggetti che rappresentano una zucchina, indicando
 per ognuna varietà, peso e lunghezza.
 Calcola quanto pesano tutte le zucchine.
  */
+
+//creare array con lista delle zucchine
+const zucchine = [
+    {
+        variety: 'nera',
+        peso: 100,
+        lunghezza: '10',
+    },
+
+    {
+        variety: 'romanesco',
+        peso: 80,
+        lunghezza: '13',
+    },
+
+    {
+        variety: 'napoletana',
+        peso: 115,
+        lunghezza: '8',
+    },
+
+    {
+        variety: 'fiorentino',
+        peso: 75,
+        lunghezza: '19',
+    },
+
+    {
+        variety: 'tonda',
+        peso: 200,
+        lunghezza: '17',
+    },
+
+    {
+        variety: 'trombetta',
+        peso: 135,
+        lunghezza: '20',
+    },
+
+    {
+        variety: 'giallo',
+        peso: 145,
+        lunghezza: '23',
+    },
+
+    {
+        variety: 'siciliano',
+        peso: 167,
+        lunghezza: '19',
+    },
+
+    {
+        variety: 'striata',
+        peso: 120,
+        lunghezza: '15',
+    },
+
+    {
+        variety: 'Crookneck',
+        peso: 280,
+        lunghezza: '25',
+    },
+
+]
+
+//stampo tutti i pesi
+zucchine.forEach((element) => {
+    console.log(element.peso);
+});
+
+    
